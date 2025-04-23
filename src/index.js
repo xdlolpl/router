b79db96985d46553ffcf292a1f79913c027116eb
xdlolpl <hubert.miluch@gmail.com>
2025-04-23 14:17:47 +0200

    react

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> 
+      <App path="/" component={App} />
+    </Router>
   </React.StrictMode>
 );
 
